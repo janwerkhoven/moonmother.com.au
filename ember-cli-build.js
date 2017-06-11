b5702abc95ef91b07cefbbce61dd1b13f56e416e
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-06-11 12:28:30 +1000

    Install and configure SASS and Autoprefixer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,21 +3,28 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    // Makes SASS listent to file changes in the component folders
+    sassOptions: {
+      includePaths: ['app/pods/components']
+    },
+    // Adds CSS browser prefixes
+    autoprefixer: {
+      browsers: ['> 1%', 'Explorer > 9', 'Firefox >= 17', 'Chrome >= 10', 'Safari >= 6', 'iOS >= 6'],
+      cascade: false,
+      remove: false
+    },
+    minifyCSS: {
+      enabled: true
+    },
+    minifyJS: {
+      enabled: true
+    },
+    sourcemaps: {
+      enabled: false
+    }
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import('vendor/google-analytics.js');
 
   return app.toTree();
 };
