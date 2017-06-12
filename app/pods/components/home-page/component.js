921470387d0fadfdb3544b831794bd6d17c1d307
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-06-12 18:11:11 +1000

    Add HTML and first logic for testimonials section

--- app/pods/components/home-page/component.js
+++ app/pods/components/home-page/component.js
@@ -1,5 +1,41 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
-  elementId: 'home-page'
+  elementId: 'home-page',
+  currentQuote: 1,
+
+  numberOfQuotes: computed(function() {
+    return this.$('#testimonials blockquote').length;
+  }),
+
+  showQuote(i) {
+
+    // To make the quotes an infinite loop
+    const max = this.get('numberOfQuotes');
+    i = i > max ? 1 : i;
+    i = i < 1 ? max : i;
+    this.set('currentQuote', i);
+    console.log(i);
+
+    // TODO: animate quotes
+    // this.$('#testimonials').addClass('something');
+    // Our use {{currentQuote}} in the template somehow
+  },
+
+  actions: {
+    nextQuote() {
+      console.log('nextQuote');
+      const current = this.get('currentQuote');
+      let toShow = current + 1;
+      this.showQuote(toShow);
+    },
+    prevQuote() {
+      console.log('prevQuote');
+      const current = this.get('currentQuote');
+      let toShow = current - 1;
+      this.showQuote(toShow);
+    }
+  }
 });
