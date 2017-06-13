a63960459c5bb7e7e21031db66320827617571db
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-06-13 22:52:05 +1000

    Add token for Instafeed

--- app/pods/components/page-footer/component.js
+++ app/pods/components/page-footer/component.js
@@ -5,11 +5,11 @@ export default Ember.Component.extend({
   elementId: 'page-footer',
   didInsertElement() {
     let feed = new Instafeed({
-      userId: '1437536913',
-      accessToken: '1437536913.1677ed0.02677d6ce703465e80498fcd44f92c54',
+      // userId: '1437536913',
+      // accessToken: '1437536913.1677ed0.02677d6ce703465e80498fcd44f92c54',
       get: 'user',
-      // userId: '2932372041',
-      // accessToken: '63814f746c034e4391c6dbfc5d7526a2',
+      userId: '2932372041',
+      accessToken: '1094777342.1233815.3fb820d15db741e798db3e7428461d43',
       sortBy: 'random',
       links: true,
       limit: 10,
@@ -19,3 +19,7 @@ export default Ember.Component.extend({
     feed.run();
   }
 });
+
+// On how to get accessToken for Instafeed
+// https://github.com/adrianengine/jquery-spectragram/wiki/How-to-get-Instagram-API-access-token-and-fix-your-broken-feed
+// https://www.instagram.com/oauth/authorize/?client_id=12338153407546998e72a429b4dd5684&redirect_uri=http://localhost:4200&response_type=token&scope=public_content
