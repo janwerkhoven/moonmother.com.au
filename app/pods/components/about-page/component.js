50cc8b497078d75ddc03596d96dcf317afadd0ef
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-06-23 14:14:11 +1200

    Add route state to header

--- app/pods/components/about-page/component.js
+++ app/pods/components/about-page/component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'main',
   elementId: 'about-page'
 });
--- app/pods/components/page-header/component.js
+++ app/pods/components/page-header/component.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
+const { computed, inject } = Ember;
+
 export default Ember.Component.extend({
+  classNameBindings: ['currentRoute'],
   tagName: 'header',
-  elementId: 'page-header'
+  elementId: 'page-header',
+  router: inject.service('-routing'),
+  currentRoute: computed.alias('router.currentRouteName')
 });
