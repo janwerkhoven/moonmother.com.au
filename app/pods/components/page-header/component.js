ef76e2598825e797ec5eaddf4a659f07a3f16c54
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-07-01 21:25:35 +1000

    Add limited CSS animation

--- app/pods/components/page-header/component.js
+++ app/pods/components/page-header/component.js
@@ -13,6 +13,25 @@ export default Ember.Component.extend({
   actions: {
     moonGlow(boolean) {
       this.set('moonGlow', boolean);
+      Ember.$('body').toggleClass('moon', boolean);
+
+      // const $pageHeader = this.$();
+      // const $constelation = this.$('#constelation');
+      //
+      // const moon = this.get('moonGlow');
+      // const pageHeaderHeight = moon ? '29vh' : '100vh';
+      // const constelationHeight = moon ? '38vh' : '100'
+      //
+      //
+      // $pageHeader.velocity('stop').velocity({
+      //   height: pageHeaderHeight
+      // }, 4000, 'easeOutExpo');
+      //
+      // $constelation.velocity('stop').velocity({
+      //   height: 38vh;
+      //   width: 38vh;
+      //   transform: translate(-19vh, -19vh);
+      // });
     }
   }
 });
