430eeebc180f375f9e75b8ca004a04e01a4ed115
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-07-01 14:28:54 +1000

    Mock up services page

--- app/pods/components/page-header/component.js
+++ app/pods/components/page-header/component.js
@@ -3,9 +3,16 @@ import Ember from 'ember';
 const { computed, inject } = Ember;
 
 export default Ember.Component.extend({
-  classNameBindings: ['currentRoute'],
   tagName: 'header',
   elementId: 'page-header',
+  classNameBindings: ['currentRoute', 'moonGlow:moon:owl'],
   router: inject.service('-routing'),
-  currentRoute: computed.alias('router.currentRouteName')
+  currentRoute: computed.alias('router.currentRouteName'),
+  moonGlow: false,
+
+  actions: {
+    moonGlow(boolean) {
+      this.set('moonGlow', boolean);
+    }
+  }
 });
--- app/pods/components/services-page/component.js
+++ app/pods/components/services-page/component.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'main',
-  elementId: 'services-page'
+  elementId: 'services-page',
+  actions: {
+    scrollToContact() {
+      Ember.$('#contact').velocity('scroll', {
+        duration: 1000,
+        easing: 'easeOutExpo',
+        begin: function() {
+          Ember.$('body').addClass('prevent-scroll');
+        },
+        complete: function() {
+          Ember.$('body').removeClass('prevent-scroll');
+        }
+      });
+    }
+  }
 });
