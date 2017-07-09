1354195e1f8872d9166b119804749fda34b710d1
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-07-09 15:46:00 +1000

    Finish up background video tweaks

---
+++ app/pods/components/background-video-section/component.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+const { computed } = Ember;
+
+export default Ember.Component.extend({
+  tagName: 'section',
+  elementId: 'background-video'
+});
---
+++ tests/integration/pods/components/background-video-section/component-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('background-video-section', 'Integration | Component | background video section', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{background-video-section}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#background-video-section}}
+      template block text
+    {{/background-video-section}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
