9228ea8666e2411cd58ae15c1c78b0b32a4dd8a4
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-07-30 17:14:25 +1000

    Make celestials move forward only

--- app/components/page-header/component.js
+++ app/components/page-header/component.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+import $ from "jquery";
+// import { computed } from "@ember/object";
+// import { inject as service } from "@ember/service";
 
 const { computed, inject } = Ember;
+
+// Big -90 0 90
+// Small -60 0 60
+
+let positions = [-90, 0, 90];
+let degrees = [-90, 0, 90];
 
 export default Ember.Component.extend({
   tagName: 'header',
@@ -10,7 +19,42 @@ export default Ember.Component.extend({
   currentRoute: computed.alias('router.currentRouteName'),
   moonGlow: false,
 
+  rotateCelestialsTo(id) {
+    if (id === 'about') {
+      positions = [-90, 0, 90];
+    } else if (id === 'services') {
+      positions = [90, -90, 0];
+    } else {
+      positions = [0, 90, -90];
+    }
+    $(`.planet`).each(function(i) {
+      if ($(this).hasClass('velocity-animating')) { return; }
+      const currentPos = degrees[i];
+      let targetPos = positions[i];
+      let newPos = targetPos < currentPos ? targetPos + 360 : targetPos;
+      $(this).velocity('stop').velocity({
+        rotateZ: `${newPos}deg`
+      }, {
+        duration: 2600,
+        easing: 'easeInOutCubic',
+        complete: function() {
+          $(this).velocity({ rotateZ: `${targetPos}deg` }, 0);
+          degrees[i] = targetPos;
+        }
+      })
+    });
+  },
+
   actions: {
+
+    collapseGalaxy(event) {
+      this.rotateCelestialsTo(event.currentTarget.id);
+    },
+
+    expandGalaxy() {
+      this.rotateCelestialsTo('index');
+    },
+
     moonGlow(boolean) {
       this.set('moonGlow', boolean);
       Ember.$('body').toggleClass('moon', boolean);
@@ -50,5 +94,9 @@ export default Ember.Component.extend({
       //   transform: translate(-19vh, -19vh);
       // });
     }
+  },
+
+  didInsertElement() {
+    this.rotateCelestialsTo(this.get('currentRoute'));
   }
 });
