37ba9b736cb579c3f21bdd240f8b2c5b7772751f
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-19 23:20:29 +1000

    Add WME content to AV page

--- app/routes/av-hire.js
+++ app/routes/av-hire.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  seoTitle: 'Audio Visual Hire melbourne',
+  seoDescription:
+    'Moon Mother helps to create stunning visual content for your event or production with our trusted audio visual hire service. Enquire now.'
+});
