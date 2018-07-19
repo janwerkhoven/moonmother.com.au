1457dc19bc5bb28617e8a171e4fb098c582d641b
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-19 23:32:05 +1000

    Add WME content to stage hire page

--- app/routes/event-hire.js
+++ app/routes/event-hire.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  seoTitle: 'Stage Lighting Truss Hire Melbourne',
+  seoDescription:
+    'Create an extraordinary light show for your upcoming event with stage lighting truss hire from Moon Mother. Enquire now by calling us.'
+});
