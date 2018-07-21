932a9b1dc1acbe844e88d82bb59383c54ab1c202
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-21 13:19:37 +1000

    Add content for Sound System page

--- app/routes/sound-system.js
+++ app/routes/sound-system.js
@@ -1,3 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  seoTitle: `Sound System`,
+  seoDescription: `Hosting a music concert, local sporting event or community festival? Moon Mother Productions offer quality PA sound systems for hire across Melbourne. Call today.`
+});
