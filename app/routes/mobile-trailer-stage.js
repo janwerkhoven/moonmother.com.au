ec9736f4fb68e1467ea70eeb473259e85b467062
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-21 13:16:56 +1000

    Add content for Mobile Trailer Stage page

--- app/routes/mobile-trailer-stage.js
+++ app/routes/mobile-trailer-stage.js
@@ -1,3 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  seoTitle: `Mobile Trailer Stage`,
+  seoDescription: `Looking for a fast, reliable solution for your next event? Contact Moon Mother Productions to hire our mobile trailer stages. Ideal for a variety of events.`
+});
