a58be465cf39359a9f3b2430bc0ac962d1b17b75
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-21 14:49:52 +1000

    Add content for Festival Stage Hire page

--- app/routes/festival-stage-hire.js
+++ app/routes/festival-stage-hire.js
@@ -1,3 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  seoTitle: 'Festival Stage Hire',
+  seoDescription: `Music concerts, sporting events or community festivals, the team at Moon Mother Productions can do it all. Contact us for indoor and outdoor festival stage hire today.`
+});
