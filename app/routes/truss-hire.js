7a67beba41068fbf3d7eb58551d7e57ee1207686
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-21 13:17:46 +1000

    Add content for Truss Hire page

--- app/routes/truss-hire.js
+++ app/routes/truss-hire.js
@@ -1,3 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  seoTitle: `Truss Hire`,
+  seoDescription: `Create an extraordinary light show for your upcoming event with stage lighting truss hire from Moon Mother. Enquire now by calling 1300 556 264.`
+});
