2c5362593242a95860df254460baa179b3e0f56c
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-07-21 13:26:22 +1000

    Update AV Hire content

--- app/routes/av-hire.js
+++ app/routes/av-hire.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  seoTitle: 'Audio Visual Hire melbourne',
-  seoDescription:
-    'Moon Mother helps to create stunning visual content for your event or production with our trusted audio visual hire service. Enquire now.'
+  seoTitle: 'AV Hire',
+  seoDescription: `Moon Mother helps to create stunning visual content for your event or production with our trusted audio visual hire service. Enquire now.`
 });
