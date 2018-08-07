fac73f64655a8345105c82af07bf6a8482c5450f
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-08-07 20:49:23 +1000

    Dry up and smoothen animation code

--- app/components/page-header/component.js
+++ app/components/page-header/component.js
@@ -3,8 +3,9 @@ import { inject as service } from '@ember/service';
 import { readOnly } from '@ember/object/computed';
 import { scrollTo } from 'moonmother/helpers/scroll-to';
 
-const totalDuration = 2600;
+const totalDuration = 2200;
 const easing = 'easeInOutCubic';
+const routes = ['index', 'about', 'services'];
 const positions = {
   full: {
     index: [0, 90, -90],
@@ -28,75 +29,51 @@ export default Component.extend({
   currentRoute: readOnly('router.currentRouteName'),
 
   collapsed: false,
+  isAnimating: false,
 
   rotateCelestialsTo(targetRoute = 'index', instant = false) {
+    // Running the animation whilst still animating, the planets will move anti-clockwise.
+    if (this.isAnimating) {
+      return;
+    }
+    this.set('isAnimating', true);
+
     const duration = instant ? 0 : totalDuration;
-
     const stance = this.collapsed ? 'half' : 'full';
-
     const currentSet = positions[stance][this.currentRoute];
     const targetSet = positions[stance][targetRoute];
 
-    // TODO: DRY up below
+    const self = this;
 
-    const currentIndex = currentSet[0];
-    const currentAbout = currentSet[1];
-    const currentServices = currentSet[2];
+    routes.forEach((route, i) => {
+      const currentRotation = currentSet[i];
+      const targetRotation = targetSet[i];
 
-    const targetIndex = targetSet[0];
-    const targetAbout = targetSet[1];
-    const targetServices = targetSet[2];
+      // Make sure that celestials only move clockwise
+      const newRotation =
+        targetRotation <= currentRotation
+          ? targetRotation + 360
+          : targetRotation;
 
-    const newIndex =
-      targetIndex <= currentIndex ? targetIndex + 360 : targetIndex;
-    const newAbout =
-      targetAbout <= currentAbout ? targetAbout + 360 : targetAbout;
-    const newServices =
-      targetServices <= currentServices ? targetServices + 360 : targetServices;
-
-    anime({
-      targets: '.planet#index',
-      rotateZ: `${newIndex}deg`,
-      delay: 0,
-      duration,
-      easing,
-      complete: function() {
-        anime({
-          targets: '.planet#index',
-          rotateZ: `${targetIndex}deg`,
-          duration: 1
-        });
-      }
-    });
-
-    anime({
-      targets: '.planet#about',
-      rotateZ: `${newAbout}deg`,
-      delay: 100, // Spread the animations for smoother frame rate
-      duration,
-      easing,
-      complete: function() {
-        anime({
-          targets: '.planet#about',
-          rotateZ: `${targetAbout}deg`,
-          duration: 1
-        });
-      }
-    });
-
-    anime({
-      targets: '.planet#services',
-      rotateZ: `${newServices}deg`,
-      delay: 200, // Spread the animations for smoother frame rate
-      duration,
-      easing,
-      complete: function() {
-        anime({
-          targets: '.planet#services',
-          rotateZ: `${targetServices}deg`,
-          duration: 1
-        });
-      }
+      anime({
+        targets: `.planet#${route}`,
+        rotateZ: `${newRotation}deg`,
+        delay: i * 100, // Spread the animations for smoother frame rate
+        duration,
+        easing,
+        complete: function() {
+          // Reset the position
+          anime({
+            targets: `.planet#${route}`,
+            rotateZ: `${targetRotation}deg`,
+            duration: 0
+          });
+          // End the animation
+          if (route === 'services') {
+            self.set('isAnimating', false);
+          }
+        }
+      });
     });
   },
 
