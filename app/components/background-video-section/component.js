a10a0f2aa777d6e7bd2db5eba2cf5b6d04057ba1
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-08-17 19:53:51 +1000

    Exclude the Youtube video if Fastboot

--- app/components/background-video-section/component.js
+++ app/components/background-video-section/component.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
   tagName: 'section',
-  elementId: 'background-video'
+  elementId: 'background-video',
+
+  fastboot: service(),
+  isFastBoot: readOnly('fastboot.isFastBoot')
 });
