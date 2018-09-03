67f3b46360c0958670d4d723a69930026411d88f
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-09-03 21:19:24 +1000

    Set event hire page meta title

--- app/routes/event-hire.js
+++ app/routes/event-hire.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  metaTitle: 'Stage Lighting Truss Hire Melbourne',
+  metaTitle: 'Event Hire',
   metaDescription: `Moon Mother is Melbourne's professional event hire company based in Melbourne. From the latest audio production equipment to marquees and fencing. Call today!`,
   metaCanonical: 'https://moonmother.com.au/event-hire'
 });
