8b620382567aea8a97fcdf30a2ff86861b0dfab4
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-02-11 17:32:47 +1100

    Update meta title to Moon Mother Productions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  metaTitle: 'Moon Mother Production',
+  metaTitle: 'Moon Mother Productions',
   metaDescription: `With an experienced, passionate crew and over 60 years experience, Moon Mother offer production services for events throughout Australia from local community gatherings to major world class events. We pride ourselves in delivering beautiful audio & visual experiences backed by top-of-the-range technology and expert management.`,
   metaCanonical: 'https://moonmother.com.au'
 });
